f3dc18f55e7f74d73a6f1c7c729d775b135e3a81
Damian Kaczmarek <rush@rushbase.net>
2014-04-07 19:08:40 +0200

    Add support for optimizing SVGs and generating spritesheets. Closes #8

--- font-awesome-svg-png.js
+++ font-awesome-svg-png.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 var fs = require('graceful-fs');
 
+var SVGO = require('svgo');
+
+var svgo = new SVGO({
+  removeViewBox: true
+});
 
 var template =
-'<svg width="{width}" height="{height}" viewBox="{shiftX} {shiftY} {width} {height}" xmlns="http://www.w3.org/2000/svg">' +
+'<svg width="{width}" height="{height}" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">' +
+'<g transform="translate({shiftX} {shiftY})">' +
 '<g transform="scale(1 -1) translate(0 -1280)">' +
 '<path d="{path}" fill="{color}" />' +
-'</g>' +
+'</g></g>' +
 '</svg>';
 
 var spawn = require('child_process').spawn;
@@ -20,14 +26,18 @@ var async = require('async');
 
 var code2name = {};
 
-var argv = require('optimist').usage("Usage: $0 -color white").demand(["color"]).describe('sizes', "Provide comma separated sizes to generate").default({sizes: "16,22,24,32,48,64,128,256"}).argv;
+var argv = require('optimist').usage("Usage: $0 -color white --sprites").describe('sizes', "Provide comma separated sizes to generate").describe('sprites', 'Generate sprites.svg to use SVG as icons (http://tympanus.net/codrops/2013/11/27/svg-icons-ftw/)').default({sizes: "16,22,24,32,48,64,128,256"}).argv;
+
+if(argv.help || (!argv.color && !argv.sprites)) {
+  return console.log(require('optimist').help());
+}
 
 var requestOptions = {
   proxy: process.env.HTTPS_PROXY
 };
 
 function run() {
-  var sizes = argv.sizes.toString().split(',');
+  var sizes = (argv.sizes || '').toString().split(',');
   function mkdir(dir) {
     try {
       fs.mkdirSync(dir);
@@ -37,14 +47,22 @@ function run() {
     }
   }
 
-  mkdir(argv.color);
-  mkdir(pathModule.join(argv.color, 'svg'));
-  mkdir(pathModule.join(argv.color, 'png'));
-  sizes.forEach(function(siz) {
-      mkdir(pathModule.join(argv.color, 'png', siz.toString()));
-  });
+  if(argv.color) {
+    mkdir(argv.color);
+    mkdir(pathModule.join(argv.color, 'svg'));
+    mkdir(pathModule.join(argv.color, 'png'));
+    sizes.forEach(function(siz) {
+        mkdir(pathModule.join(argv.color, 'png', siz.toString()));
+    });
+  }
 
   var PIXEL = 128;
+
+  var outSvgSheet;
+  if(argv.sprites) {
+    outSvgSheet = fs.createWriteStream(pathModule.join('sprites.svg'));
+    outSvgSheet.write('<svg height="0" width="0" style="position:absolute;margin-left: -100%;">\n');
+  }
 
   function generateIcon(name, path, params, cb) {
     var out = template.substr(0);
@@ -52,8 +70,8 @@ function run() {
 
     function getTemplate(options) {
       params = extend({}, params, {
-        shiftX: -(14*PIXEL - params.advWidth)/2 - options.paddingLeft,
-        shiftY: -2*PIXEL - options.paddingTop,
+        shiftX: -(-(14*PIXEL - params.advWidth)/2 - options.paddingLeft),
+        shiftY: -(-2*PIXEL - options.paddingTop),
         width: 14*PIXEL + options.paddingLeft + options.paddingRight,
         height: 14*PIXEL + options.paddingBottom + options.paddingTop,
       });
@@ -97,20 +115,64 @@ function run() {
 
     console.log("Generating icon", name);
 
-    async.eachSeries(sizes, function(siz, cb) {
-      var rsvgConvert;
-      rsvgConvert = spawn('rsvg-convert', ['-f', 'png', '-w', siz, '-o', pathModule.join(argv.color, 'png', siz.toString(), name+'.png')]);
-      rsvgConvert.stdin.end(getTemplate(optionsForSize(siz)));
-      rsvgConvert.once('error', cb);
-      rsvgConvert.once('exit', cb);
-    }, cb);
-    var outSvg = fs.createWriteStream(pathModule.join(argv.color, 'svg', name + '.svg'));
-    outSvg.end(getTemplate({
-      paddingTop: 0,
-      paddingLeft: 0,
-      paddingBottom: 0,
-      paddingRight: 0
-    }));
+    var workChain = [];
+
+    if(argv.color) {
+      workChain.push(function(cb) {
+        async.eachSeries(sizes, function(siz, cb) {
+          var rsvgConvert;
+          rsvgConvert = spawn('rsvg-convert', ['-f', 'png', '-w', siz, '-o', pathModule.join(argv.color, 'png', siz.toString(), name+'.png')]);
+          rsvgConvert.stdin.end(getTemplate(optionsForSize(siz)));
+          rsvgConvert.once('error', cb);
+          rsvgConvert.once('exit', cb);
+        }, cb);  
+      });
+    }
+
+    if(argv.color) {
+
+      workChain.push(function(cb) {
+
+        var outSvg = fs.createWriteStream(pathModule.join(argv.color, 'svg', name + '.svg'));
+        svgo.optimize(getTemplate({
+          paddingTop: 0,
+          paddingLeft: 0,
+          paddingBottom: 0,
+          paddingRight: 0
+        }), function(result) {
+
+          outSvg.end(result.data);
+          cb();
+        });
+
+      });
+    }
+    if(argv.sprites) {
+      workChain.push(function(cb) {
+
+        svgo.optimize(getTemplate({
+          paddingTop: 0,
+          paddingLeft: 0,
+          paddingBottom: 0,
+          paddingRight: 0
+        }), function(result) {
+
+          var m = result.data.match('(<path.*\/>)');
+
+          var svgPath = m[1].replace('path', 'path id="' + name + '"');
+          if(outSvgSheet)
+            outSvgSheet.write(svgPath.replace(/\s*fill="[^"]+"/, '') + '\n');
+
+          cb();
+        });
+
+      });
+    }
+
+
+    async.parallel(workChain, cb);
+  
+
   }
 
 
@@ -152,6 +214,10 @@ function run() {
           console.log("Make sure 'rsvg-convert' command is available in the PATH");
           return console.log("Error occured:", err);
         }
+        if(outSvgSheet) {
+          outSvgSheet.end('<\/svg>\n');
+        }
+
         console.log("All generated");
       });
 
